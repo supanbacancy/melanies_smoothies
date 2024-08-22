fd7cf07e802ba496c52bd2eef2bd5deb47b0da7b
supanbacancy <supan.shah@bacancy.com>
2024-08-22 16:38:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,11 @@ if ingredients_list:
         st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
 
         st.subheader(fruit_choosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        if search_on==None:
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+            
+        else: 
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
